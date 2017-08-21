f2f40b461356aea7b866e4ae5bccdcb07b3868d1
haitaozhao <haitaozhao@ecust.edu.cn>
2017-08-21 12:58:52 +0800

    w

--- Matlab_code/myConstructW.m
+++ Matlab_code/myConstructW.m
@@ -67,7 +67,7 @@ for i=1:col1
         if(i==j)
             phi=1;
         else
-            phi=0;
+            hi=0;
         end
         C(i,j)=alpha(i)*Z(i,j)+alpha(j)*Z(j,i)-alpha(i)*alpha(j)-alpha(i)*phi;
         if C(i,j)<=0
